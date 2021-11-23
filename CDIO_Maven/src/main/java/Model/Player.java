e4c052eb719d93c8071cea80c1719ee4980005ad
EspenUlff <s215831@student.dtu.dk>
2021-11-23 14:42:06 +0100

    Added attributes in preperation for more game functions (Jail, board position, bankrupt)

--- CDIO_Maven/src/main/java/Model/Player.java
+++ CDIO_Maven/src/main/java/Model/Player.java
@@ -1,15 +1,21 @@
 package Model;
 
-import gui_fields.GUI_Player;
+import Model.Game.Config;
 
 public class Player {
     private String name;
-    private int money;
-    //private boolean inJail; skal eftertjekkes med kunden
+    private int money = Config.START_MONEY;
+    private int position = 0;
+    private boolean inJail = false;
+
+    private boolean outOfJailFree = false;
+    private int jailTime = 0;
+
+    private boolean bankrupt = false;
+
 
     public Player(String name) {
         this.name = name;
-        this.money = 35;
     }
 
     public String getName() {
@@ -19,8 +25,47 @@ public class Player {
     public int getMoney() {
         return money;
     }
-
     public void addMoney(int value){
         this.money += value;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public boolean isInJail() {
+        return inJail;
+    }
+
+    public void setInJail(boolean inJail) {
+        this.inJail = inJail;
+    }
+
+    public boolean isOutOfJailFree() {
+        return outOfJailFree;
+    }
+
+    public void setOutOfJailFree(boolean outOfJailFree) {
+        this.outOfJailFree = outOfJailFree;
+    }
+
+    public int getJailTime() {
+        return jailTime;
+    }
+
+    public void setJailTime(int jailTime) {
+        this.jailTime = jailTime;
+    }
+
+    public boolean isBankrupt() {
+        return bankrupt;
+    }
+
+    public void setBankrupt(boolean bankrupt) {
+        this.bankrupt = bankrupt;
     }
 }
